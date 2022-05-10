fb3e7c5489178b1433222675bbab1015262dbea5
Mihash08 <mihsasandomirskiy@gmail.com>
2022-05-10 18:47:25 +0300

    First GUI test

--- main.py
+++ main.py
@@ -1,44 +1,112 @@
-import kivy
-
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
 from kivy.lang import Builder
-from kivy.uix.popup import Popup
-from kivy.uix.screenmanager import  Screen, ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
-
-from telethon import TelegramClient, functions
-from telethon.errors import SessionPasswordNeededError
-from telethon.tl.functions.messages import GetHistoryRequest
-
-dev_username = "Mihash08"
-api_id = 9770358
-api_hash = "e9d3d03202a6d1c827187ac8cbc604b9"
-dev_phone = "+79251851096"
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from database import DataBase
 
 
-class Widgets(Widget):
-    def btn(self):
-        show_popup()
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
 
-class P(FloatLayout):
+    def submit(self):
+        if self.namee.text != "" and self.email.text != "" and self.email.text.count("@") == 1 and self.email.text.count(".") > 0:
+            if self.password != "":
+                db.add_user(self.email.text, self.password.text, self.namee.text)
+
+                self.reset()
+
+                sm.current = "login"
+            else:
+                invalidForm()
+        else:
+            invalidForm()
+
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
+
+
+class LoginWindow(Screen):
+    phone = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.phone.text, self.password.text):
+            MainWindow.current = self.phone.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.phone.text = ""
+        self.password.text = ""
+
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "login"
+
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+        self.created.text = "Created On: " + created
+
+
+class WindowManager(ScreenManager):
     pass
 
-class MyApp(App):
+
+def invalidLogin():
+    pop = Popup(title='Invalid Login',
+                  content=Label(text='Invalid username or password.'),
+                  size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+
+def invalidForm():
+    pop = Popup(title='Invalid Form',
+                  content=Label(text='Please fill in all inputs with valid information.'),
+                  size_hint=(None, None), size=(400, 400))
+
+    pop.open()
+
+
+kv = Builder.load_file("my.kv")
+
+sm = WindowManager()
+db = DataBase("data.txt")
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"),MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+
+class MyMainApp(App):
     def build(self):
-        return Widgets();
+        return sm
 
-
-def show_popup():
-    show = P()
-    popupWindow = Popup(title="ERROR", content=show, size_hint=(None,None), size=(400,400))
-
-    popupWindow.open()
 
 if __name__ == "__main__":
-    MyApp().run()
+    MyMainApp().run()
